10217dbdd3afe3de73c4b0943c4cd2335817819b
ismailacikyurek <ismailackyurek@gmail.com>
2023-03-27 02:30:01 +0300

    sceneDelegate edit

--- pokemonCase/App/SceneDelegate.swift
+++ pokemonCase/App/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: scene)
-        let mainController = ViewController()
+        let mainController = ListViewController()
         let navigationController = UINavigationController(rootViewController: mainController)
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
