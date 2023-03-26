e9ebe50da4bd1ee406157dbc2d8aaf479980e5b7
ismailacikyurek <ismailackyurek@gmail.com>
2023-03-27 02:29:47 +0300

    extension UIView add

--- pokemonCase/Component/UIView/extension+UIview.swift
+++ pokemonCase/Component/UIView/extension+UIview.swift
@@ -40,3 +40,20 @@ extension UIView {
     }
     
 }
+
+extension UIView {
+    func addSubviews(_ views: UIView...) {
+        for i in views {
+            addSubview(i)
+        }
+    }
+}
+
+extension UIView {
+    func roundCornersView(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+}
