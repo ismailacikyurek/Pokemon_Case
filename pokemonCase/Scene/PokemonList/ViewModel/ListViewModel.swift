bd1e4a119bfca3b37363297c33e6caacc9564e41
ismailacikyurek <ismailackyurek@gmail.com>
2023-03-27 18:31:34 +0300

    listViewModel add

---
+++ pokemonCase/Scene/PokemonList/ViewModel/ListViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  ListViewModel.swift
+//  pokemonCase
+//
+//  Created by İSMAİL AÇIKYÜREK on 27.03.2023.
+//
+
+import Kingfisher
+import UIKit
+
+//MARK: Protocols
+protocol ListViewModelProtocol: AnyObject {
+    func didError(_ error: String)
+    func didAllListSuccess()
+}
+
+class ListViewModel {
+    
+    weak var delegate : ListViewModelProtocol?
+    private let service : WebService = WebService()
+    
+    var pokemonList : PokemonListModel?
+    var pokemonDetail : PokemonDetailModel?
+    
+    func fetchAllPokemonList() {
+        service.fetchAllPokomenList { response in
+            self.pokemonList = response
+            self.delegate?.didAllListSuccess()
+        } onFail: { error in
+            self.delegate?.didError(error!.localizedDescription)
+        }
+    }
+    
+    func fetchPokemonDetail(_ url : String) {
+        service.fetchPokemonDetai(url: url) { response in
+            self.pokemonDetail = response
+        } onFail: { error in
+            self.delegate?.didError(error!.localizedDescription)
+        }
+    }
+
+}
+
